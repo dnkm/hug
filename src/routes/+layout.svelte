d439b1e53cf11964dba5f003b5ba88072a72565b
Daniel Kim <chipperday@gmail.com>
2025-09-30 05:08:26 +0000

    class demo

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -9,16 +9,23 @@
 	<link rel="icon" href={favicon} />
 </svelte:head>
 
-<nav>
-	<div class="bg-blue-500 h-8"></div>
-	<a href="/">home</a>
-	<a href="/missions">missions and activities</a>
-</nav>
+<div class="min-h-screen flex flex-col">
+	<nav>
+		<div class="h-8 bg-primary"></div>
+		<div class="container mx-auto flex justify-between py-4 text-lg">
+			<div>LOGO</div>
+			<a href="/">home</a>
+			<a href="/missions">missions and activities</a>
+			<a href="/">?</a>
+			<a href="/">?</a>
+			<a href="/">?</a>
+			<button class="btn btn-accent">Join Us!</button>
+		</div>
+	</nav>
 
-<main>
-	{@render children?.()}
-</main>
+	<main class="flex-1">
+		{@render children?.()}
+	</main>
 
-<footer>
-	footer
-</footer>
+	<footer class="bg-primary text-primary-content h-[200px]">footer</footer>
+</div>
--- src/routes/missions/+page.svelte
+++ src/routes/missions/+page.svelte
@@ -1,8 +1,52 @@
-<h1 class="text-2xl">Mission and Vision</h1>
+<div class="banner text-white">
+	<div class="container mx-auto">
+		<div class="w-[450px]">
+			<h2 class="text-4xl">Uplifting Local Facilities</h2>
+			<h2 class="text-4xl">And Programs</h2>
+			<div class="my-2 flex h-[2px]">
+				<div class="w-10 bg-primary"></div>
+				<div class="flex-1 bg-white"></div>
+			</div>
+			<div>Promotiong healthy, supportive community relationships</div>
+			<button class="btn my-3 btn-accent">Join US!</button>
+		</div>
+	</div>
+</div>
 
-<img src="/images/1000013259.jpg" alt="">
+<main class="">
+	<section class="">
+		<div class="container bg-red-100 mx-auto">
+			<h3>Our Mission</h3>
+			<p>
+				H.U.G is a non-profit volunteer organization founded in Orange County in 2014. It is known
+				as an impactful student-run community service organization led by high-achieving high school
+				students from across Southern California. HUG pursues the mission of making the world a
+				better place and organizes and carries out numerous events and activities throughout the
+				year including cleaning parks and beaches, hosting garage sales and other fundraising for
+				students in need, and working to improve lives internationally.
+			</p>
+		</div>
+	</section>
+	<section class="bg-gray-300">
+		<div class="container mx-auto">
+			<h3>Our Mission</h3>
+			<p>
+				H.U.G is a non-profit volunteer organization founded in Orange County in 2014. It is known
+				as an impactful student-run community service organization led by high-achieving high school
+				students from across Southern California. HUG pursues the mission of making the world a
+				better place and organizes and carries out numerous events and activities throughout the
+				year including cleaning parks and beaches, hosting garage sales and other fundraising for
+				students in need, and working to improve lives internationally.
+			</p>
+		</div>
+	</section>
+</main>
 
-<p>
-H.U.G is a non-profit volunteer organization founded in Orange County in 2014.  It is known as an impactful student-run community service organization led by high-achieving high school students from across Southern California. HUG pursues the mission of making the world a better place and organizes and carries out numerous events and activities throughout the year including cleaning parks and beaches, hosting garage sales and other fundraising for students in need, and working to improve lives internationally. 
-</p>
-
+<style>
+	.banner {
+		height: 300px;
+		background: url('/images/1000013259.jpg');
+		background-position: center;
+		background-size: cover;
+	}
+</style>
